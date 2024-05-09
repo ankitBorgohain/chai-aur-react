4b10bf6aa55139da0b5eec4f92669c4dca524ea8
ankitBorgohain <ankit.borgohain98@gmail.com>
2024-05-09 23:58:31 +0530

    Created Pages/ Resolving Bugs

--- 12Megablog/src/components/index.js
+++ 12Megablog/src/components/index.js
@@ -3,8 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
-import Button from "./Button";
-
+import RTE from './RTE'
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
 
 
@@ -14,5 +18,11 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 
 }
